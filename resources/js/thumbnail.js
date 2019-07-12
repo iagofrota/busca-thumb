f1939d04bb170b8439b366238e4842eea7b81023
Iago Olímpio Frota <iagofrota10@gmail.com>
2019-07-11 22:08:41 -0300

    Melhorando o código javascript

--- resources/js/thumbnail.js
+++ resources/js/thumbnail.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var url = $("#url");
-
-    var ctrlDown = false,
-        ctrlKey = 17,
-        cmdKey = 91,
-        vKey = 86,
-        cKey = 67;
+    let ctrlDown = false;
+    const url = $('#url');
+    const ctrlKey = 17;
+    const cmdKey = 91;
+    const vKey = 86;
+    const cKey = 67;
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
 
     $(document).keydown(function (e) {
         if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
@@ -18,20 +18,18 @@ $(document).ready(function () {
             $(this).change()
     })
         .change(function () {
-            console.log("Deu certo pegar " + url.val())
             thumbnail(url.val())
         })
 
     function thumbnail(url) {
-        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-        var match = url.match(regExp);
-        href = 'http://img.youtube.com/vi/' + match[7] + '/0.jpg';
+        let match = url.match(regExp);
+        let href = `http://img.youtube.com/vi/${match[7]}/0.jpg`;
 
         if (match && match[7].length == 11) {
-            b1.src = 'http://img.youtube.com/vi/' + match[7] + '/0.jpg';
+            b1.src = href;
             b1.title = 'Clique para baixar';
-            botao.href = 'http://img.youtube.com/vi/' + match[7] + '/0.jpg';
+            botao.href = href;
             botao.download = 'Miniatura.jpg';
         }
     }
-})
+});
